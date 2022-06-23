60a290bd8639cebf4e2e7da86d575d6172acaa01
d9beuD <d9beuD@users.noreply.github.com>
2022-06-23 09:51:45 +0200

    Try to fix action

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,6 @@
 # abort on errors
 set -e
 
-git config --global user.email "vincent.bathelier@protonmail.com"
-git config --global user.name "Vincent Bathelier"
-
 # build
 npm run docs:build
 
@@ -14,15 +11,3 @@ cd docs/.vitepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:d9beuD/com.d9beud.knowledges.git main:gh-pages
-
-cd -
