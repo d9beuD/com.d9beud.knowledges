b871ee92800f2fa68d1f57ef2d783ab07da33b5e
Vincent Bathelier <53569029+d9beuD@users.noreply.github.com>
2022-07-08 20:20:31 +0200

    Update edit link

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -26,7 +26,7 @@ export default defineConfig({
     },
 
     editLink: {
-      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path'
+      pattern: 'https://github.com/d9beuD/com.d9beud.knowledges/edit/main/docs/:path'
     },
   }
 })
